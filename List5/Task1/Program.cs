1919cd96032bfc11e3448538a642d24401080e6f
Jakub Kozanecki <jkozanecki@op.pl>
2023-11-21 21:41:19 +0100

    added exception handling

--- List5/Task1/Program.cs
+++ List5/Task1/Program.cs
@@ -12,47 +12,66 @@ namespace Task1
                 Console.WriteLine("Podaj współczynniki równania kwadratowego ax^2 + bx + c = 0");
 
                 Console.Write("a: ");
-                double a = Convert.ToDouble(Console.ReadLine());
+                double a;
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Błędne dane dla 'a'. Spróbuj ponownie.");
+                    continue; // Kontynuuj pętlę od nowa
+                }
 
                 Console.Write("b: ");
-                double b = Convert.ToDouble(Console.ReadLine());
+                double b;
+                if (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine("Błędne dane dla 'b'. Spróbuj ponownie.");
+                    continue; // Kontynuuj pętlę od nowa
+                }
 
                 Console.Write("c: ");
-                double c = Convert.ToDouble(Console.ReadLine());
+                double c;
+                if (!double.TryParse(Console.ReadLine(), out c))
+                {
+                    Console.WriteLine("Błędne dane dla 'c'. Spróbuj ponownie.");
+                    continue; // Kontynuuj pętlę od nowa
+                }
 
                 var solution = calculate(a, b, c);
 
                 switch (solution.solutionsCount)
                 {
                     case 0:
-                        Console.WriteLine("No real solutions");
+                        Console.WriteLine("Brak rzeczywistych rozwiązań");
                         break;
                     case 1:
-                        Console.WriteLine("One real solution: x1 = {0}", solution.x1);
+                        Console.WriteLine("Jedno rzeczywiste rozwiązanie: x1 = {0}", solution.x1);
                         break;
                     case 2:
-                        Console.WriteLine($"Two real solutions: x1 = {solution.x1}, x2 = {solution.x2}");
+                        Console.WriteLine($"Dwa rzeczywiste rozwiązania: x1 = {solution.x1}, x2 = {solution.x2}");
                         break;
                 }
-                Console.Write("Do you want to continue? If so type yes: ");
-                String input = Console.ReadLine().ToUpper();
-                if (input != "YES")
+
+                Console.Write("Czy chcesz kontynuować? Jeśli tak, wpisz 'tak': ");
+                string input = Console.ReadLine().ToUpper();
+                if (input != "TAK")
                 {
                     finish = true;
                 }
             }
-
         }
 
         public static (int solutionsCount, string x1, string x2) calculate(double a, double b, double c)
         {
-            if (a != 0 && b != 0)
+            if (a != 0)
             {
                 double delta = b * b - 4 * a * c;
+
                 if (delta > 0)
                 {
-                    double x1 = (Math.Sqrt(delta) - b) / (2 * a);
-                    double x2 = (-Math.Sqrt(delta) - b) / (2 * a);
+                    double sqrtDelta = Math.Sqrt(delta);
+
+                    double x1 = (-b + sqrtDelta) / 2 * a;
+                    double x2 = (-b - sqrtDelta) / 2 * a;
+
                     return (2, x1.ToString(), x2.ToString());
                 }
                 else if (delta == 0)
@@ -60,25 +79,20 @@ namespace Task1
                     double x = -b / (2 * a);
                     return (1, x.ToString(), "absent");
                 }
-                else
+                else if (b == 0)
                 {
-                    return (0, "absent", "absent");
-                }
-            }
-            else if (a != 0 && b == 0)
-            {
-                if ((-c / a) >= 0)
-                {
-                    double x1 = Math.Sqrt(-c / a);
-                    double x2 = -Math.Sqrt(-c / a);
-                    return (2, x1.ToString(), x2.ToString());
+                    double x = Math.Sqrt(-c / a);
+                    double x1 = x;
+                    double x2 = -x;
+
+                    return (x >= 0 ? 2 : 0, x1.ToString(), x2.ToString());
                 }
                 else
                 {
                     return (0, "absent", "absent");
                 }
             }
-            else if (a == 0 && b != 0)
+            else if (b != 0)
             {
                 double x = c / b;
                 return (1, x.ToString(), "absent");
--- List5/Task1/obj/Debug/net5.0/Task1.AssemblyInfo.cs
+++ List5/Task1/obj/Debug/net5.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370267e094a234953777f6c36866b4129d094708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdc257d082f728e4e1491ddf935724ad2dcba66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
